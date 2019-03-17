44bd46394260a8ac7879c24350cdbf62cff884c7
Dexter Richards <Dexter.Richards6@gmail.com>
2019-03-17 14:55:43 -0400

    Fixed placeholder scoreboard being super janky

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import PencilModeOptions from '../containers/pencilModeOptions';
 
 const Scoreboard = (props) => {
-	const pencilModeButton = (
-		<button className={`btn btn-${props.pencil_mode? "primary" : "secondary"}`} onClick={props.pencilModeToggle}>Pencil Mode {props.pencil_mode? "On" : "Off"}</button>
-	);
-	const giveUpButton = (
-		<button className="btn btn-danger" onClick={props.giveUp}>Give Up</button>
-	);
-
 	return (
-		<div className="container innerContainer">
-			<h2>Level {props.level}</h2>
-			{props.children}
-			{pencilModeButton}
-			{giveUpButton}
-			{props.pencil_mode && <PencilModeOptions />}
+		<div className="container">
+			SCORE GOES HERE
 		</div>
 	);
 }
 
-Scoreboard.propTypes = {
-	pencilModeToggle: PropTypes.func.isRequired,
-	giveUp: PropTypes.func.isRequired,
-
-	level: PropTypes.number.isRequired,
-	children: PropTypes.any.isRequired,
-	pencil_mode: PropTypes.bool.isRequired
-};
+Scoreboard.propTypes = {};
 
 Scoreboard.defaultProps = {};
 
